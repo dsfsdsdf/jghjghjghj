ba3742b906ba328f04655c08588d83edac009648
dsfsdsdf <50211086+dsfsdsdf@users.noreply.github.com>
2019-05-04 11:11:04 +0200

    Create imgur.js

---
+++ commands/imgur.js
@@ -0,0 +1,34 @@
+const { Discord, RichEmbed} = require('discord.js');
+module.exports.run = async (bot, message, args) => {
+  console.log("imgur");
+
+var Attachment = (message.attachments).array()
+const msg = args.join(" ")
+console.log(Attachment[0].url)
+//  if(sayMessage === "") {
+//message.channel.send("test");}
+console.log("1")
+console.log(message.attachments)
+if (Attachment[0].url === undefined) {
+  console.log("erreur")
+  return
+}
+imgur.setAPIUrl('https://api.imgur.com/3/');
+imgur.uploadUrl(Attachment[0].url)
+    .then(function (json) {
+        console.log(json.data.link);
+        message.channel.send(json.data.link)
+    })
+    .catch(function (err) {
+        console.error(err.message);
+    });
+}
+
+  }
+
+
+
+
+module.exports.help = {
+    name: "upl"
+  }
